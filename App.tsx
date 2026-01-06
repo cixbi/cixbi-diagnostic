49c860f1b59c79e79ecab8ab819e0e9af91c079a
cixbi <robyn@innervoicecreative.com>
2026-01-06 10:04:26 +0200

    Remove React import as it's not needed

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { QUESTIONS, PROFILES } from './constants';
 import { Answers, Scores, SavedProgress, ProfileType } from './types';
 import { IntroScreen } from './components/IntroScreen';
